68fb3b3f5ddf238d2be2fbb9edb823b6dfc5ca98
Woodse07 <sewoods@tcd.ie>
2018-11-06 12:40:25 +0000

    Possible solution to question 1

--- Assignment_2/hw2.pl
+++ Assignment_2/hw2.pl
@@ -1,4 +1,19 @@
-string --> [].
-string --> u, u, string, v.
-u --> [0].
-v --> [1].
+% QUESTION 1
+% u2v where num of 0s in u is twice the number of 1s in v.
+
+string --> string(_).
+string(N) --> u(N), two, v(N).
+
+two --> [2].
+
+u(0) --> [].
+u(succ(succ(N))) --> [0], u(N1).
+u(N) --> [1], u(N).
+
+v(0) --> [].
+v(succ(N)) --> [1], v(N1).
+v(N) --> [0], v(N).
+
+
+% QUESTION 2
+
