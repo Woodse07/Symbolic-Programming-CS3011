9d2619221acd96e5f6cfb5e7454c8a2a63b033cb
Woodse07 <sewoods@tcd.ie>
2018-11-30 13:03:16 +0000

    Finished question 3

--- Exam_Papers/2015/2015.pl
+++ Exam_Papers/2015/2015.pl
@@ -35,3 +35,30 @@ next1(A, [H|_]) :- A is H.
 next1(A, B, [H|T]) :- A is H, next1(B, T); next1(A, B, T).
 
 
+
+
+
+
+u2v --> u(Count), [2], v(Count).
+
+u(0) --> [].
+u(succ(Count)) --> [1], u(Count).
+u(Count) --> [0], u(Count).
+
+v(0) --> [].
+v(Count) --> [1], v(Count).
+v(succ(succ(Count))) --> [0], v(Count).
+
+
+mkList(1, [1]).
+mkList(N, [N|T]) :- NN is N - 1, mkList(NN, T).
+
+sum_list([], 0).
+sum_list([H|T], Sum) :- sum_list(T, OldSum), Sum is OldSum + H.
+
+
+half(Half) --> {Half >= 1, Full is Half*2, mkList(Full, L)}, genList(L, Full).
+
+genList(L, Full) --> {Full >= 0, member(X, L), Rest is Full-X}, [X], genList(L, Rest).
+genList(_, 0) --> [].
+
