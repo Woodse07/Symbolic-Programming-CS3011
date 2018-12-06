6e5b5f128c24b90eff06cab3d40d4be85e864aa2
Woodse07 <sewoods@tcd.ie>
2018-12-06 23:37:30 +0000

    Finished 2017 Q2

---
+++ Exam_Papers/2017/2017.pl
@@ -0,0 +1,82 @@
+
+
+lessSome([H|T], [H1|T1]) :- number(H), someCycle(H, [H1|T1]) ; lessSome(T, T1).
+someCycle(Num, [H|T]) :- number(H), Num < H ; someCycle(Num , T).
+
+
+lessAll([], L).
+lessAll([H|T], L) :- number(H), allCycle(H, L), lessAll(T, L).
+allCycle(_, []).
+allCycle(Num, [H|T]) :- number(H), Num < H, allCycle(Num , T).
+
+
+
+
+append1([], List, List).
+append1([H|T], List2, [H|List3]) :- append1(T, List2, List3).
+
+
+
+
+sum1([], 0).
+sum1([H|T], Sum) :- sum1(T, OldSum), Sum is OldSum + H.
+
+
+
+length1(List, N) :- lengthhelper(List, 0, N).
+lengthhelper([], N, N).
+lengthhelper([H|T], Acc, N) :- NewAcc is Acc + 1, lengthhelper(T, NewAcc, N).
+
+
+
+split(X, List, Small, Big) :- 
+	member(X, List),
+	splithelper(X, List, [], [], Small, Big).
+
+splithelper(_, [], Small, Big, Small, Big).
+splithelper(X, [H|T], S, B, Small, Big) :-
+	H < X, splithelper(X, T, [H|S], B, Small, Big);
+	H > X, splithelper(X, T, S, [H|B], Small, Big);
+	H = X, splithelper(X, T, S, B, Small, Big).
+
+
+median(List, Median) :- 
+	length1(List, N), 
+	1 is mod(N, 2),
+	I is floor(N/2),
+	findNum(List, I, Median).
+
+findNum([H|T], 0, H).
+findNum([H|T], I, Median) :- findNum(T, II, Median), I is II + 1.
+
+
+
+remove(X, List, Rest) :- member(X, List), listBuilder(X, List, [], Rest).
+
+listBuilder(X, [], Rest, Rest).
+listBuilder(X, [H|T], Acc, Rest) :-
+	X \= H -> listBuilder(X, T, [H|Acc], Rest);
+	listBuilder(X, T, Acc, Rest).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
